c56b7a2ea39c713dc840cc86269f150e2d6857c3
diogosalvadorb <60317215+diogosalvadorb@users.noreply.github.com>
2021-01-21 23:00:59 -0300

    rotação imagens e imagem model

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,6 +1,6 @@
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-
+import {Imagem } from './image.model'
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
@@ -18,17 +18,41 @@ import { Component, OnInit } from '@angular/core';
     ])
   ]
 })
+
 export class BannerComponent implements OnInit {
 
   public estado: string = 'visivel'
 
+  public imagens: Imagem[] =[
+    { estado: 'visivel', url: '/assets/banner-acesso/img_1.png' },
+    { estado: 'oculto', url: '/assets/banner-acesso/img_2.png' },
+    { estado: 'oculto', url: '/assets/banner-acesso/img_3.png' },
+    { estado: 'oculto', url: '/assets/banner-acesso/img_4.png' },
+    { estado: 'oculto', url: '/assets/banner-acesso/img_5.png' }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => this.logicaRotacao(), 3000)
   }
 
-  public toggleEstado(): void{
-    this.estado = this.estado === 'visivel' ? 'oculto': 'visivel'
-  }
+  public logicaRotacao(): void {
+    
+    let proximaImagem: number
+    //oculta imagem
+    for(let i:number = 0; i <=4; i++) {
+      if (this.imagens[i].estado === 'visivel') {
+        this.imagens[i].estado = 'oculto'
 
+        proximaImagem = i === 4 ? 0 : i + 1
+        break
+      }
+
+    }
+    //exibir proxima imagem
+    this.imagens[proximaImagem].estado = 'visivel'
+
+    setTimeout(() => this.logicaRotacao(), 3000)
+  }
 }
